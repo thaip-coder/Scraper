8d985fc16e1cb9b43244acbae1dc743701b7efbe
Thai <thaiphan90@yahoo.com>
2019-10-02 16:01:57 -0500

    added folders

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 //Required Dependencies
 var cheerio = require("cheerio");
 var axios = require("axios");
-var rp = require("request-promise");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
 var express = require("express");
 var app = express();
 
+var db = require("./models");
 
 console.log("\n***********************************\n" +
             "Grabbing every thread name and link\n" +
             "from reddit's webdev board:" +
             "\n***********************************\n");
 
-axios.get("https://www.ultimatetennisstatistics.com/rankingsTable").then(function(res) {
+axios.get("https://www.polygon.com/gaming").then(function(res) {
     var $ = cheerio.load(res.data);
 
     var results = [];
